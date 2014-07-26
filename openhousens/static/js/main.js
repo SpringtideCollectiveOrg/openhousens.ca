dd5bbcad2ac339f474cc8c084cc543bf44c0f403
James McKinney <james@slashpoundbang.com>
2014-07-26 17:03:03 -0400

    Expandable speeches, closes #6

--- openhousens/static/js/main.js
+++ openhousens/static/js/main.js
@@ -14,20 +14,40 @@ $(function () {
       'personalbar=0,toolbar=0,scrollbars=1,resizable=1');
   });
 
-  $('.tablesorter').tablesorter({
-    sortList: [[1, 0]]
-  , textExtraction: function (node) {
-      if (node.className.indexOf('modified') !== -1) {
-        return node.children[0].getAttribute('datetime');
+  var $tablesorter = $('.tablesorter');
+  if ($tablesorter.length) {
+    $tablesorter.tablesorter({
+      sortList: [[1, 0]]
+    , textExtraction: function (node) {
+        if (node.className.indexOf('modified') !== -1) {
+          return node.children[0].getAttribute('datetime');
+        }
+        else {
+          return node.innerHTML;
+        }
       }
-      else {
-        return node.innerHTML;
+    , widgets: ['filter']
+    , widgetOptions: {
+        filter_columnFilters: false
+      , filter_external: '.form-filter input'
       }
-    }
-  , widgets: ['filter']
-  , widgetOptions: {
-      filter_columnFilters: false
-    , filter_external: '.form-filter input'
+    });
+  }
+
+  $('.truncatable').each(function () {
+    if (this.clientHeight < this.scrollHeight) {
+      $('<div class="expand-fade"></div>').appendTo(this);
+      $('<div class="expand-link"><span class="glyphicon glyphicon-chevron-down"></span> see more</div>').click(function () {
+        $(this).hide()
+          .parent().children('.expand-fade').hide()
+          .parent().animate({
+            maxHeight: '800px'
+          }, {
+            easing: 'linear'
+          , complete: function () {
+            $(this).css('max-height', 'none');
+          }})
+      }).appendTo(this);
     }
   });
 });
